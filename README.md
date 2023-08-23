fc5af18f4d562e02184245cc1b18dcaa50bea3fb
aymanyassinn <jcodes19@gmail.com>
2023-08-23 20:25:25 +0300

    JDraggable First

--- README.md
+++ README.md
@@ -1,39 +1,36 @@
-<!--
-This README describes the package. If you publish this package to pub.dev,
-this README's contents appear on the landing page for your package.
+# JDraggableView
 
-For information about how to write a good package README, see the guide for
-[writing package pages](https://dart.dev/guides/libraries/writing-package-pages).
-
-For general information about developing packages, see the Dart guide for
-[creating packages](https://dart.dev/guides/libraries/create-library-packages)
-and the Flutter guide for
-[developing packages and plugins](https://flutter.dev/developing-packages).
--->
-
-TODO: Put a short description of the package here that helps potential users
-know whether this package might be useful for them.
+JDraggableView is a versatile Flutter widget that allows you to create a draggable view with various customization options. You can easily integrate it into your Flutter app to create interactive and flexible user interfaces.
 
 ## Features
 
-TODO: List what your package can do. Maybe include images, gifs, or videos.
+- Create a draggable view with customizable behavior.
+- Configure the appearance of the draggable view, including the app bar, body, and more.
+- Supports pull-to-refresh functionality.
+- Contains Scroll Controller .
+- Built-in state management using Provider.
 
-## Getting started
+## Installation
 
-TODO: List prerequisites and provide or point to information on how to
-start using the package.
+To use JDraggableView in your Flutter project, add it to your `pubspec.yaml`:
 
-## Usage
-
-TODO: Include short and useful examples for package users. Add longer examples
-to `/example` folder.
-
-```dart
-const like = 'sample';
+```yaml
+dependencies:
+  jdraggable_view: 0.0.1
 ```
-
+## Usage
+Import JDraggableView in your file
+```dart
+import 'package:jdraggable_view/jdraggable_view.dart';
+```
+Then Use it like a Scaffold:
+```dart
+ @JDraggableView(
+  title: Text('Draggable View Example'),
+  body: <Widget>[
+  ],
+)
+```
 ## Additional information
 
-TODO: Tell users more about the package: where to find more information, how to
-contribute to the package, how to file issues, what response they can expect
-from the package authors, and more.
+Provided By [Just Codes Developers](https://jucodes.com/)
--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:jdraggable_view/jdraggable_view.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,100 +12,139 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'JDraggable View',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const HomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<HomePage> createState() => _HomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
+class _HomePageState extends State<HomePage> {
+  ScrollController scrollController = ScrollController();
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+    return JDraggableView(
+      leading: const Icon(Icons.arrow_back_ios),
+      title: const Text("Collapsed View"),
+      actions: [
+        IconButton(onPressed: () {
+          scrollController.animateTo(scrollController.position.minScrollExtent - 200, duration: const Duration(milliseconds: 300), curve: Curves.ease);
+        }, icon: const Icon(Icons.arrow_downward_sharp)),
+      ],
+      expandedHeader: Container(
+        color: Colors.blue,
+        child: Center(
+          child: Text(
+            "Expanded View",
+            style: Theme.of(context)
+                .textTheme
+                .displayMedium!
+                .copyWith(color: Colors.white70),
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      controller: scrollController,
+      expandedHeaderBottomBar:  Row(
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.end,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          IconButton(onPressed: () {
+            scrollController.animateTo(scrollController.position.maxScrollExtent, duration: const Duration(milliseconds: 300), curve: Curves.ease);
+          }, icon: const Icon(Icons.arrow_upward)),
+        ],
+      ),
+      body: [
+        for(int i = 0; i<20; i++)
+          Card(
+            color: Colors.white70,
+            child: ListTile(
+              leading: CircleAvatar(
+                child: Text("$i"),
+              ),
+              title: const Text("Title"),
+              subtitle: const Text("Details"),
+            ),
+          )
+      ],
+      headerExpandedHeight: 0.2,
+      fullyStretchable: true,
+      stretchMaxHeight: 0.3,
+      onRefresh: ()async{
+        await Future.delayed(const Duration(milliseconds: 1500));
+      },
+      // expandedBody: RefreshIndicator(child: child, onRefresh: onRefresh),
+      expandedBody: Stack(
+        children: [
+          Container(color: Colors.black),
+          SafeArea(
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.teal,
+                borderRadius: BorderRadius.circular(50),
+              ),
+              child: Stack(
+                children: [
+                  Positioned(
+                      top: 20,
+                      right: 20,
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          CircleAvatar(
+                            backgroundColor: Colors.black.withOpacity(.5),
+                            child: const Icon(
+                              Icons.ac_unit,
+                              color: Colors.white,
+                            ),
+                          )
+                        ],
+                      )),
+                  Positioned(
+                    bottom: 50,
+                    child: SizedBox(
+                      width: MediaQuery.of(context).size.width,
+                      child: Row(
+                        mainAxisSize: MainAxisSize.max,
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          ClipRRect(
+                            borderRadius: BorderRadius.circular(50),
+                            child: Container(
+                              color: Colors.grey[300],
+                              height: 100,
+                              width: 100,
+                              alignment: Alignment.center,
+                              child: const CircleAvatar(
+                                radius: 40,
+                                backgroundColor: Colors.blue,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+      backgroundColor: Colors.white,
+      appBarColor: Colors.teal,
     );
   }
 }
